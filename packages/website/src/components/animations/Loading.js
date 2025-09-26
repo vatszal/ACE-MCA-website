03763739de926b9ff08ca2d8510616c18720ebaf
vatszal <vatsalmitt05@gmail.com>
2025-09-26 13:51:23 +0530

    deploy debugging

--- packages/website/src/components/animations/Loading.js
+++ packages/website/src/components/animations/Loading.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import loading from './loading.json'
-import Lottie from 'react-lottie'
 import { FlexCenter } from '../../styles/sharedStyles'
 
-export default function Loading ({ loop = true, autoplay = true }) {
+export default function Loading({ loop = true, autoplay = true }) {
+  const [isClient, setIsClient] = useState(false)
+  
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
   const defaultOptions = {
     loop,
     autoplay,
@@ -13,6 +18,15 @@ export default function Loading ({ loop = true, autoplay = true }) {
     }
   }
 
+  if (!isClient) {
+    return (
+      <FlexCenter>
+        <div style={{ height: '200px', width: '200px' }}>Loading...</div>
+      </FlexCenter>
+    )
+  }
+
+  const Lottie = require('react-lottie')
   return (
     <FlexCenter>
       <Lottie options={defaultOptions} height={200} width={200} />
